621b9969dbb1b5fa0d28e9a2dd6666c184532438
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-11-14 03:33:23 -0300

    Criar routes/__init__.py

---
+++ routes/__init__.py
@@ -0,0 +1,8 @@
+# routes/__init__.py
+# Este arquivo transforma a pasta 'routes' em um pacote Python
+# e importa todos os módulos de rota para que o Flask os encontre.
+
+from . import routes_core
+from . import routes_products
+from . import routes_markets
+from . import routes_prices
