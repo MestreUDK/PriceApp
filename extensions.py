448a5e19a11d768633b67ffe3ef7d87ff98167d6
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-11-14 15:46:42 -0300

    Atualizar o extensions.py

--- extensions.py
+++ extensions.py
@@ -4,6 +4,12 @@
 # importações circulares.
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager # <-- MUDANÇA 1
+from flask_bcrypt import Bcrypt      # <-- MUDANÇA 2
 
 # Cria a instância do db, mas não a liga a nenhum app ainda
 db = SQLAlchemy()
+
+# --- MUDANÇA 3: Instancia as novas extensões ---
+login_manager = LoginManager()
+bcrypt = Bcrypt()
